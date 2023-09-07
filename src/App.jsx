a80bb4090356932316229c702767faa2ff0fdba6
Jhonny Claure <claurejhonny188@gmail.com>
2023-09-07 08:40:14 +0200

    V 0.06

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import {Header} from './components/Header.jsx';
+import { Board } from './components/TicTacToe.jsx';
 import './styles/main.css';
 
 export function App () {
     return (
-        <Header />
+        <>
+            <Header />
+            <Board />
+        </>
     )
 }
