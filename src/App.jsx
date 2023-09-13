a4129060352fc3dc7b9550d02c22aaaa52b13474
Jhonny Claure - Laptop <claurejhonny188@gmail.com>
2023-09-13 10:44:20 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export function App () {
   return (
-    <Router basename='/wolrd-project-nl'>
+    <Router>
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
